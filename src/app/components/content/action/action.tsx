d658e1547f823079241d7bced9560496602d6f4b
Martins Kozlovskis <mkozlovskis@gmail.com>
2024-03-26 22:55:26 +0200

    added sending ETH to an address

--- src/app/components/content/action/action.tsx
+++ src/app/components/content/action/action.tsx
@@ -8,25 +8,32 @@ import { AllBalances } from '@/app/components/content/action/all-balances';
 
 // hooks
 import { useGetAllBalances } from '@/app/components/content/action/hooks/get-all-balances.hook';
+import { TOKEN_NAME } from '@/app/constants';
 
 /**
  * Container for showing the balances and sending funds to the specified address.
  */
 export const Action = () => {
-  const {  balances, loading} = useGetAllBalances();
+  const { balances, loading } = useGetAllBalances();
 
-  if (loading) {
+  if (loading || !balances) {
     // TODO: replace with proper loading spinner
     // TODO: add proper styling
     return <div>Loading balances...</div>;
   }
+
   // TODO: add proper styling
   return (
-    <div>
-      <BalanceContext.Provider value={balances}>
+    <BalanceContext.Provider value={balances}>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}>
         <AllBalances/>
-        <Send/>
-      </BalanceContext.Provider>
-    </div>
+        <Send balance={balances[TOKEN_NAME].balance}/>
+      </div>
+    </BalanceContext.Provider>
+
   );
 };
--- src/app/components/content/action/send/send.tsx
+++ src/app/components/content/action/send/send.tsx
@@ -1,15 +1,64 @@
+import { FC } from 'react';
+import {
+  type BaseError,
+  useSendTransaction,
+  useWaitForTransactionReceipt,
+} from 'wagmi';
+import { parseEther } from 'viem';
+
+// constants
+import { RECIPIENT_ADDRESS } from '@/app/constants';
+
+// types
+import { BalanceValue } from '@/app/types/index.type';
+
 /**
  * Sends the funds to the recipient.
  */
-export const Send = () => {
+export const Send: FC<{
+  balance: BalanceValue
+}> = ({ balance }) => {
+  const {
+    data: hash,
+    error,
+    isPending,
+    sendTransaction,
+  } = useSendTransaction();
+  const {
+    isLoading: isConfirming,
+    isSuccess: isConfirmed,
+  } = useWaitForTransactionReceipt({ hash });
+
+  /**
+   * Send button click handler.
+   */
   const onClick = () => {
-    // TODO: add send functionality
+    if (isPending) {
+      return;
+    }
+
+    // TODO: send the token not ETH instead
+    sendTransaction({ to: RECIPIENT_ADDRESS, value: parseEther(String(balance)) });
   };
 
-  // TODO: add proper styling
   return (
-    <button onClick={onClick}>
-      Send
-    </button>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '0.125rem',
+    }}>
+      <button
+        onClick={onClick}
+        disabled={isPending}
+      >
+        {isPending ? 'Confirming...' : 'Send'}
+      </button>
+      {hash && <div>Transaction Hash: {hash}</div>}
+      {isConfirming && <div>Waiting for confirmation...</div>}
+      {isConfirmed && <div>Transaction confirmed.</div>}
+      {error && (
+        <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+      )}
+    </div>
   );
-}
+};
